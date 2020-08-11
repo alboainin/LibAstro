19f415a6205ae6fbc73639cb0ba3112e233a4bcb
Ali Alboainin <44374885+AliAlboainin96@users.noreply.github.com>
2020-08-11 18:55:25 +0300

    Added WindowResizeEvent

--- src/libastro/event/WindowEvent.h
+++ src/libastro/event/WindowEvent.h
@@ -1,4 +1,18 @@
-#include "event.h"
+#include "Event.h"
 
 namespace astro {
+    class WindowResizeEvent : public Event
+    {
+    public:
+        WindowResizeEvent(unsigned int rows, unsigned int columns)
+            :m_Columns(columns), m_Rows(rows) {}
+
+        unsigned int GetColumns() const { return m_Columns; }
+        unsigned int GetRows()    const { return m_Rows;    }
+     
+            
+    private:
+        unsigned int m_Columns, m_Rows;
+    }
+
 }
